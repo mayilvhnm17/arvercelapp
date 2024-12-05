4fbb8f5bb3c92b902317e7238c462ac404557792
mayilvhnm17 <mayilvhnm17@GMAIL.COM>
2024-12-06 00:40:16 +0530

    <Mayil> test

--- api/models/save-model.js
+++ api/models/save-model.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
 	try {
 		// Verify the user's access token
 		const user = authenticateUser(token);
+        console.log(JSON.stringify(user));
 		if (!user) {
 			return res.status(401).json({ error: "Unauthorized" });
 		}
