3ac5defe388fed8f1d65edd02587e7da1310a3b7
mayilvhnm17 <mayilvhnm17@GMAIL.COM>
2024-12-09 22:23:16 +0530

    .

--- api/auth/google.js
+++ api/auth/google.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 
 	try {
 		await connectToDatabase();
-		console.log(tokenId);
+		console.log(req.body);
 		// Verify Google token
 		const ticket = await client.verifyIdToken({
 			idToken: tokenId,
